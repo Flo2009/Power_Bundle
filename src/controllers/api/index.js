9d228e688e78310e63ca74b381a6f46e9e1ce2bf
Tuba Muktadir <tuba@Tubas-MBP.lan>
2024-08-01 19:49:31 -0400

    updating files

--- src/controllers/api/index.js
+++ src/controllers/api/index.js
@@ -1,9 +1,14 @@
 const router = require('express').Router();
+
 const customerRoutes = require('./customerRoutes');
 const productRoutes = require('./productRoutes');
+const orderRoutes = require ('./orderRoutes');
+const orderItemRoutes = require ('./orderItemRoutes');
 
-router.use('/customers', userRoutes);
+router.use('/customers', customerRoutes);
 router.use('/products', productRoutes);
+router.use('/orders', orderRoutes);
+router.use('/orderItems', orderItemRoutes);
 
 module.exports = router;
 
